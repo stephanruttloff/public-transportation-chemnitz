d8f061fcf8946bfc4a8a99378a4535371cbdf0aa
Stephan Ruttloff <stephan@ruttloff.org>
2015-07-24 18:16:14 +0200

    code formatting

--- server.js
+++ server.js
@@ -11,22 +11,22 @@ app.listen(process.argv[2])
 
 function GetDepartures(req, res)
 {
-	var stationId = req.params.stationId;
-	// example url: 	http://www.cvag.de/eza/mis/stops/station/CAG-326
-	// example referer: http://www.cvag.de/eza/liste.html?station=CAG-326
+    var stationId = req.params.stationId;
+    // example url:     http://www.cvag.de/eza/mis/stops/station/CAG-326
+    // example referer: http://www.cvag.de/eza/liste.html?station=CAG-326
 
-	request({
-		uri: "http://www.cvag.de/eza/mis/stops/station/" + stationId,
-		headers: {
-			'Connection': 'keep-alive',
-			'Accept': 'application/json, text/javascript, */*; q=0.01',
-			'X-Requested-With': 'XMLHttpRequest',
-			'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
-			'Referer': 'http://www.cvag.de/eza/liste.html?station=' + stationId,
-			'Accept-Encoding': 'gzip, deflate, sdch',
-			'Accept-Language': 'de,en-US;q=0.8,en;q=0.6,de-DE;q=0.4'
-		}
-	}, function(error, response, body) {
-		res.json(body);
-	});
+    request({
+        uri: "http://www.cvag.de/eza/mis/stops/station/" + stationId,
+        headers: {
+            'Connection': 'keep-alive',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
+            'Referer': 'http://www.cvag.de/eza/liste.html?station=' + stationId,
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'de,en-US;q=0.8,en;q=0.6,de-DE;q=0.4'
+        }
+    }, function(error, response, body) {
+        res.json(body);
+    });
 }
