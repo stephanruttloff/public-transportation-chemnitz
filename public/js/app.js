bfb53bad58e6ff21881771bb17fc06ce1903b997
Stephan Ruttloff <stephan@ruttloff.org>
2015-07-24 12:14:34 +0200

    switch to gMaps, dynamic departure timer

--- public/js/app.js
+++ public/js/app.js
@@ -1,63 +1,78 @@
-var app = angular.module("cvag", ["leaflet-directive", "geolocation"]);
-app.controller("main", [ "$scope", "$http", "leafletData", "leafletBoundsHelpers", "geolocation", function($scope, $http, leafletData, leafletBoundsHelpers, geolocation) {
+var app = angular.module("cvag", ["geolocation", "ngMap", "ngMaterial", "angularMoment", "timer"]);
+app.controller("main", [ "$scope", "$http", "$sce", "$compile", "geolocation", "moment", function($scope, $http, $sce, $compile, geolocation, moment) {
 
-	var bounds = leafletBoundsHelpers.createBoundsFromArray([
-        [ 50.6, 12.5 ],
-        [ 50.95, 13.1 ]
-    ]);
-    angular.extend($scope, {
-    	bounds: bounds,
-        //maxbounds: bounds,
-        center: {},
-        defaults: {
-            scrollWheelZoom: false
-        },
-        layers: {
-            baselayers: {
-                googleTerrain: {
-                    name: 'Google Terrain',
-                    layerType: 'TERRAIN',
-                    type: 'google'
-                },
-                googleHybrid: {
-                    name: 'Google Hybrid',
-                    layerType: 'HYBRID',
-                    type: 'google'
-                },
-                googleRoadmap: {
-                    name: 'Google Streets',
-                    layerType: 'ROADMAP',
-                    type: 'google'
-                }
+    $scope.bottom = {
+        title: "",
+        content: "",
+        show: false,
+        height: 0
+    }
+
+    $scope.top = {
+        height: 100
+    }
+
+    $scope.attachEventListener = function(marker, station)
+    {
+        marker.station = station;
+        google.maps.event.addListener(marker, 'mousedown', function() {
+            $scope.$apply(function(){
+                var header = "<b>" + marker.station.displayName + "</b><br>";
+                var loading = $compile("<md-progress-circular md-mode='indeterminate' style='margin-left:auto;margin-right:auto;'></md-progress-circular>")($scope)[0].outerHTML;
+                var content = header + loading;
+                $scope.map.infowindow.open($scope.map, marker);
+                $scope.map.infowindow.setContent(content);
+
+                $http.get('departures/' + marker.station.id).success(function(data) {
+                    var d = angular.fromJson(data);
+                    var now = new moment(d.now);
+                    var table = "<table><tr><th>Linie</th><th>Richtung</th><th>Abfahrt</th></tr>";
+                    for(var i = 0; i < d.stops.length; i++)
+                    {
+                        table += "<tr>"
+
+                        var diff = new moment(d.stops[i].actualDeparture).diff(now, 'Minutes');
+                        table += "<td>" + d.stops[i].line + "</td>";
+                        table += "<td>" + d.stops[i].destination + "</td>";
+                        table += "<td text-align='right'><timer max-time-unit=\"'minute'\" end-time='" + d.stops[i].actualDeparture + "'>{{mminutes}}:{{sseconds}}</timer></td>";
+                        //table += "<td>" + diff + "</td>";
+
+                        table += "</tr>"
+                    }
+                    table += "</table>";
+                    var content = "<div>" + header + table + "</div>"
+                    content = $compile(content)($scope);
+                    $scope.map.infowindow.setContent(content[0]);
+                });
+            })
+        });
+    }
+
+    $scope.$on('mapInitialized', function(event, map) {
+        map.infowindow = new google.maps.InfoWindow({
+            content: ""
+        });
+
+        geolocation.getLocation().then(function(data){
+            var myLatLng = new google.maps.LatLng(data.coords.latitude, data.coords.longitude);
+            map.setCenter(myLatLng);
+            map.setZoom(15);
+        });
+
+        $http.get('resources/stops.json').success(function(data) {
+            var stations = data.stations;
+            var image = new google.maps.MarkerImage('img/CVAG@2x.png', null, null, null, new google.maps.Size(25,41));;
+
+            for(i = 0; i < stations.length; i++)
+            {
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(stations[i].latitude, stations[i].longitude),
+                    map: map,
+                    title: stations[i].displayName,
+                    icon: image
+                });
+                $scope.attachEventListener(marker, stations[i]);
             }
-        }
-    });
-
-
-    $http.get('resources/stops.json').success(function(data) {
-    	var stations = (data.stations);
-	   	var markers = {};
-	   	for(i = 0; i < stations.length; i++)
-	   	{
-	   		var message = '<b>' + stations[i].displayName + '</b><br>' +
-	   					  '<iframe src="http://www.cvag.de/eza/liste.html?station=' + stations[i].id + '"></iframe>';
-	   		markers[i] = {
-	   			lat:stations[i].latitude,
-	   			lng:stations[i].longitude,
-	   			message: message,
-	   			focus: false,
-	   			draggable: false
-	   		};
-	   	}
-	   	angular.extend($scope, {
-	        markers: markers
-	    });
-	});
-	geolocation.getLocation().then(function(data){
-    	$scope.center = {
-            lat: data.coords.latitude,
-            lng: data.coords.longitude,
-            zoom: 17
-	    }
+        });
     });
 }]);
--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 var express = require('express')
 var serveStatic = require('serve-static')
+var request = require("request")
+var moment = require("moment")
 
 var app = express()
 
+app.get('/departures/:stationId', GetDepartures)
+
 app.use(serveStatic('./public', {'index': ['default.html', 'default.htm']}))
 app.listen(80)
+
+function GetDepartures(req, res)
+{
+	var stationId = req.params.stationId;
+	// example url: 	http://www.cvag.de/eza/mis/stops/station/CAG-326
+	// example referer: http://www.cvag.de/eza/liste.html?station=CAG-326
+
+	request({
+		uri: "http://www.cvag.de/eza/mis/stops/station/" + stationId,
+		headers: {
+			'Connection': 'keep-alive',
+			'Accept': 'application/json, text/javascript, */*; q=0.01',
+			'X-Requested-With': 'XMLHttpRequest',
+			'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
+			'Referer': 'http://www.cvag.de/eza/liste.html?station=' + stationId,
+			'Accept-Encoding': 'gzip, deflate, sdch',
+			'Accept-Language': 'de,en-US;q=0.8,en;q=0.6,de-DE;q=0.4'
+		}
+	}, function(error, response, body) {
+		res.json(body);
+	});
+}
