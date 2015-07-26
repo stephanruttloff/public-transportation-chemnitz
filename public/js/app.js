4b3a6a9eed1d909c2fcbec19a0884c47b8a49c74
Stephan Ruttloff <stephan@ruttloff.org>
2015-07-26 16:36:08 +0200

    added places search box

--- public/js/app.js
+++ public/js/app.js
@@ -8,14 +8,14 @@ app.controller("main", [ "$scope", "$http", "$sce", "$compile", "$interval", "$t
         loading: true
     }
 
+    $scope.mapInitDone = false;
+
     $scope.attachEventListener = function(marker, station)
     {
         marker.station = station;
         google.maps.event.addListener(marker, 'mouseup', function() {
             if($scope.reactToMarker)
             {
-                console.log($scope.reactToMarker);
-
                 $scope.map.setOptions({draggable: false});
                 $scope.$apply(function(){
                     if($scope.map.infowindow.getContent() === "")
@@ -58,6 +58,53 @@ app.controller("main", [ "$scope", "$http", "$sce", "$compile", "$interval", "$t
     }
 
     $scope.$on('mapInitialized', function(event, map) {
+        var markers = [];
+        var input = /** @type {HTMLInputElement} */(document.getElementById('pac-input'));
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        var searchBox = new google.maps.places.SearchBox(/** @type {HTMLInputElement} */(input));
+        google.maps.event.addListener(searchBox, 'places_changed', function() {
+            var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+              return;
+            }
+            for (var i = 0, marker; marker = markers[i]; i++) {
+              marker.setMap(null);
+            }
+
+            // For each place, get the icon, place name, and location.
+            markers = [];
+            var bounds = new google.maps.LatLngBounds();
+            for (var i = 0, place; place = places[i]; i++) {
+                var image = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+
+                // Create a marker for each place.
+                var marker = new google.maps.Marker({
+                    map: map,
+                    icon: image,
+                    title: place.name,
+                    position: place.geometry.location
+                });
+
+                markers.push(marker);
+
+                bounds.extend(place.geometry.location);
+            }
+
+            map.fitBounds(bounds);
+        });
+
+        google.maps.event.addListener(map, 'bounds_changed', function() {
+            var bounds = map.getBounds();
+            searchBox.setBounds(bounds);
+        });
+
         google.maps.event.addListener(map, 'zoom_changed', function() {
             $scope.reactToMarker = false;
             $timeout($scope.doReactToMarker, 500);
@@ -107,5 +154,7 @@ app.controller("main", [ "$scope", "$http", "$sce", "$compile", "$interval", "$t
                 $scope.attachEventListener(marker, stations[i]);
             }
         });
+
+        $scope.mapInitDone = true;
     });
 }]);
